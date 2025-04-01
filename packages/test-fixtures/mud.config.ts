4cbf51e935b9b4e82f33f92af74127180c439054
Jérémie COLOMBO <5773637+beaukode@users.noreply.github.com>
2025-04-01 19:40:50 +0200

    Update script to a more general testnet env and set it to pyrope

--- packages/test-fixtures/mud.config.ts
+++ packages/test-fixtures/mud.config.ts
@@ -1,7 +1,9 @@
 import { defineWorld } from "@latticexyz/world";
 
+const namespace = process.env.TEST_FIXTURES_NAMESPACE || process.env.DEFAULT_NAMESPACE;
+
 export default defineWorld({
-  namespace: "beauKode_dev",
+  namespace: namespace,
   tables: {
     TestTable1: {
       schema: {
